b73241a997c99cbbffa23cf18768613cd8ad5300
David <david17sego@gmail.com>
2018-01-23 13:35:02 +0100

    Step 27: Naming server now discovers 3 microservices

--- currency-conversion-service/src/main/java/com/davidsegoviatomas/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/davidsegoviatomas/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,14 @@ package com.davidsegoviatomas.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableFeignClients("com.davidsegoviatomas.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- currency-exchange-service/src/main/java/com/davidsegoviatomas/currencyexchangeserver/CurrencyExchangeServerApplication.java
+++ currency-exchange-service/src/main/java/com/davidsegoviatomas/currencyexchangeserver/CurrencyExchangeServerApplication.java
@@ -2,8 +2,10 @@ package com.davidsegoviatomas.currencyexchangeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServerApplication {
 
 	public static void main(String[] args) {
